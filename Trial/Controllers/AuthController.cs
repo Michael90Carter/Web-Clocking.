5a219a2fad56d475a155f9210542687ced94a922
Michael <Michael@DESKTOP-6395OH7>
2024-02-28 16:27:04 +0300

    Add Signin and signout pages

---
+++ Trial/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Trial.Models;
+
+namespace Trial.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpGet("signin")]
+        public IActionResult SignIn(string returnUrl = null)
+        {
+            
+            {
+                if (returnUrl != null)
+                    return RedirectToAction(returnUrl);
+                
+                        return Redirect("/");
+            }
+
+            return View();
+        }
+        [HttpGet("signout")]
+        [Authorize]
+      /*  public async Task<IActionResult> Signout()
+        {
+            if (SessionManager.IsAuthenticated)
+            {
+                await SessionManager.SignoutAsync();
+                return Redirect("Account/signin");
+            }
+
+            return View(new SignUpModel());
+        }*/
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Trial/Controllers/HomeController.cs
+++ Trial/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace Trial.Controllers
             return View();
         }
 
+        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
